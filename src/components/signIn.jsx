fe429a34c98115381d4bad107979764276b159e5
WknightJr <austinbouchard6@gmail.com>
2025-02-16 14:35:54 -0500

    Update signIn.jsx

--- src/components/signIn.jsx
+++ src/components/signIn.jsx
@@ -1,9 +1,11 @@
 import * as React from 'react'
-
+import { Context } from "./App"
+//This imports Context so we can use it in this file//
 
 export const SignInButton = () => {
 
-    const [signedIn,setSignedIn] = React.useState(false)
+    const [signedIn,setSignedIn] = useContext(Context)
+//The SignInButton is now using the Context we are importing.//
 
     return (
         <button onClick={() => setSignedIn(!signedIn)}>
