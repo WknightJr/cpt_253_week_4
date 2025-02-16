2a101c1857a03a90812a90b787ac2956a8158e6a
WknightJr <austinbouchard6@gmail.com>
2025-02-16 14:36:25 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,26 @@ import './App.css'
 
 import { Nav } from './components/nav'
 
+export const Context = React.createContext();
+//This is setting up Context so I can use it to pass the states.//
+
 function App() {
 
   const [signedIn, setSignedIn] = useState(false)
+//This is the same state as the button. This is so the state can be used outside of just the button.//
 
+//The nav has been wrapped in Contect.Provider tags so that its state can be changed.//
   return (
-    <>
+    <Context.Provider value={[signedIn, setSignedIn]}>
       <Nav />
       <h1>
         {
           signedIn ? "Signed Out" : "Signed In" 
         }
       </h1>
-    </>
+    </Context.Provider>
   )
 }
 
 export default App
+
